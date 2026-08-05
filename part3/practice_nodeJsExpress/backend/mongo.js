a8fcd84b37b2afe4afcb4820336032af003b7c3c
daniil <2404752@edu.karelia.fi>
2026-08-05 14:31:14 +0300

    cmd_mongo_practice

---
+++ part3/practice_nodeJsExpress/backend/mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://admin:${password}@clust.vlxmraa.mongodb.net/noteApp?appName=Clust`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url, { family: 4 });
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
+const note = new Note({
+  content: "Admin is here",
+  important: true,
+});
+
+/*
+note.save().then((result) => {
+  console.log(result.content);
+  console.log("note saved!");
+  mongoose.connection.close();
+});
+*/
+Note.find({}).then((result) => {
+  result.forEach((note) => {
+    console.log(note);
+  });
+  mongoose.connection.close();
+});
