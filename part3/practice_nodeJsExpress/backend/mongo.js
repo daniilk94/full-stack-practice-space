d8d889498561527bcd796be9b789757395c321d3
daniil <2404752@edu.karelia.fi>
2026-08-07 10:35:13 +0300

    Eslint config introduced

--- part3/practice_nodeJsExpress/backend/mongo.js
+++ part3/practice_nodeJsExpress/backend/mongo.js
@@ -1,30 +1,30 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-  console.log("give password as argument");
-  process.exit(1);
+  console.log('give password as argument')
+  process.exit(1)
 }
 
-const password = process.argv[2];
-const url = `mongodb+srv://admin:${password}@clust.vlxmraa.mongodb.net/noteApp?appName=Clust`;
+const password = process.argv[2]
+const url = `mongodb+srv://admin:${password}@clust.vlxmraa.mongodb.net/noteApp?appName=Clust`
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 
-mongoose.connect(url, { family: 4 });
+mongoose.connect(url, { family: 4 })
 
 const noteSchema = new mongoose.Schema({
   content: String,
   important: Boolean,
-});
+})
 
-const Note = mongoose.model("Note", noteSchema);
-
-const note = new Note({
-  content: "Admin is here",
-  important: true,
-});
+const Note = mongoose.model('Note', noteSchema)
 
 /*
+const note = new Note({
+  content: 'Admin is here',
+  important: true,
+})
+
 note.save().then((result) => {
   console.log(result.content);
   console.log("note saved!");
@@ -34,7 +34,7 @@ note.save().then((result) => {
 
 Note.find({}).then((result) => {
   result.forEach((note) => {
-    console.log(note);
-  });
-  mongoose.connection.close();
-});
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
