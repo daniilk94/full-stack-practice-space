cfcce525a9e3b690b0e49b114e571cd915659dc1
daniil <2404752@edu.karelia.fi>
2026-08-07 11:13:38 +0300

    fix

--- part3/practice_nodeJsExpress/backend/models/note.js
+++ part3/practice_nodeJsExpress/backend/models/note.js
@@ -7,7 +7,7 @@ const url = process.env.MONGODB_URI
 console.log('Connecting to MongoDB...')
 mongoose
   .connect(url, { family: 4 })
-  .then((result) => {
+  .then(() => {
     console.log('Connected to MongoDB')
   })
   .catch((error) => {
