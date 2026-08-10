d4535c3b66ddfbb45e672e1e257564d73f01e768
daniil <2404752@edu.karelia.fi>
2026-08-10 11:16:26 +0300

    Structure refactoring (different export used in middleware)

--- notesApp/backend/models/note.js
+++ notesApp/backend/models/note.js
@@ -1,19 +1,5 @@
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false)
-
-const url = process.env.MONGODB_URI
-
-console.log('Connecting to MongoDB...')
-mongoose
-  .connect(url, { family: 4 })
-  .then(() => {
-    console.log('Connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('Error connecting to MongoDB:', error.message)
-  })
-
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
